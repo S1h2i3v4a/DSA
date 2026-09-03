b6c952ed1560d640d347dbff5300916f750fe5c1
Shivam Keshari <shivamkeshari990@gmail.com>
2026-09-03 13:19:26 +0530

    Add Subarray Sum Equals K using Prefix Sum HashMap Frequency approach

---
+++ Arrays/560_Subarray_Sum_Equals_K.java
@@ -0,0 +1,25 @@
+﻿/*
+ * Problem Name: Subarray Sum Equals K
+ * Platform: LeetCode (560) / GeeksforGeeks
+ * Difficulty: Medium
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+
+import java.util.HashMap;
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, count = 0;
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
